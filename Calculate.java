b9048700db6a9ffc3eb445baa813f393a7e517e3
Igor Zolov <daledreams@gmail.com>
2019-09-10 22:30:26 +0300

    add all arithmetic operations

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,30 @@
 public class Calculate{
     public static void main(String[] args){
+			int firstOperand = Integer.valueOf(args[1]);
+			int secondOperand = Integer.valueOf(args[2]);
+        	switch (args[0]){
+				case -a:
 
-        	System.out.println("Welcome dear, God");
+					int result = firstOperand+secondOperand;
+					System.out.println("addition: " + firstOperand+" "+secondOperand);
+					System.out.println("result: "+result);
+					break;
+				case -s:
+					int result = firstOperand-secondOperand;
+					System.out.println("subtraction: "+firstOperand+" "+secondOperand);
+					System.out.println("result: "+result);
+					break;
+				case -m:
+					int result = firstOperand*secondOperand;
+					System.out.println("multiplication: " + firstOperand+" "+secondOperand);
+					System.out.println("result: "+result);
+					break;
+				case -d:
+					int result = firstOperand/secondOperand;
+					System.out.println("division: " + firstOperand+" "+secondOperand);
+					System.out.println("result: "+result);
+					break;
+			}
         
     }
 }
---
+++ src/ru/skaii/Calculator.java
@@ -0,0 +1,16 @@
+package ru.skaii;
+
+public class Calculate {
+
+    private String prefix;
+    private int firtstOperand;
+    private int secondOperand;
+
+    public Calculate(String prefix, int firtstOperand, int secondOperand) {
+        this.prefix = prefix;
+        this.firtstOperand = firtstOperand;
+        this.secondOperand = secondOperand;
+    }
+
+    public int 
+}
